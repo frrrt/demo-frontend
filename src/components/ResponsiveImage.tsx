0a2bfc2e429de2f150cf019df4d9c1be7e210e06
Moritz Thomas <mzt@live.de>
2025-03-01 21:21:19 +0100

    Responsive image component

---
+++ src/components/ResponsiveImage.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Image from "next/image";
+import { Box, SxProps, Theme } from "@mui/material";
+import { breakpoints, spacing } from "@/styles/consts";
+
+const ResponsiveImage = ({
+  src,
+  alt,
+  width,
+  height,
+  priority = false,
+  quality = 75,
+  sx = {},
+  ...rest
+}: {
+  src: string;
+  alt?: string;
+  width: number;
+  height: number;
+  priority?: boolean;
+  quality?: number;
+  sizes?: string;
+  sx?: SxProps<Theme>;
+}) => {
+  return (
+    <Box
+      sx={{
+        "& > img": { width: "100%", height: "auto" },
+        ...sx,
+      }}
+      {...rest}
+    >
+      <Image
+        src={src}
+        alt={alt || ""}
+        width={width}
+        height={height}
+        sizes={`(max-width: ${breakpoints.sm}px) calc(100vw - ${
+          spacing * 6
+        }px), (max-width: ${breakpoints.md}px) calc(100vw - ${
+          spacing * 8
+        }px), 852px`}
+        priority={priority}
+        quality={quality}
+      />
+    </Box>
+  );
+};
+
+export default ResponsiveImage;

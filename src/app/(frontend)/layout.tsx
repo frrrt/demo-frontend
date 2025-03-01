955f3a99555152a922439b4f5f92f8f78b78d7b5
Moritz Thomas <mzt@live.de>
2025-03-01 23:36:48 +0100

    Moved frontend files to group

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,7 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
+import theme from "../../theme";
 import { AppBar, Typography, Container } from "@mui/material";
 import Menu from "@/components/Menu";
 import Link from "next/link";

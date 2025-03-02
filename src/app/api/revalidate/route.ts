f5c2e7f55fae29540569f2512c2dfbc09ae4aab8
Moritz Thomas <mzt@live.de>
2025-03-02 17:22:18 +0100

    Switched to tag based caching/revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,16 +1,15 @@
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log("Revalidation request", body);
-  const { token, path } = body;
+  const { token, tag } = body;
 
   if (token !== process.env.REVALIDATION_TOKEN) {
     return Response.json({ message: "Invalid token" }, { status: 401 });
   }
 
   try {
-    revalidatePath(path === "index" ? "/" : path);
+    revalidateTag(tag);
     return Response.json({ revalidated: true, now: Date.now() });
   } catch (error) {
     console.log("Error revalidating", error);

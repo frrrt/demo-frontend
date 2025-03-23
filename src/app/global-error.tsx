cc7d3e1e87487387f87585b115c0eb93c7835b4a
Moritz Thomas <mzt@live.de>
2025-03-23 19:58:21 +0100

    Removed obsolete import

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,13 +1,7 @@
 "use client";
 import { useEffect } from "react";
 
-export default function GlobalError({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);

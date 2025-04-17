7ae3583ba39c644c86eecead00a092437b8a1cc5
Moritz Thomas <mzt@live.de>
2025-04-17 22:55:44 +0200

    Infinite caching time

--- src/app/(frontend)/[locale]/[slug]/page.tsx
+++ src/app/(frontend)/[locale]/[slug]/page.tsx
@@ -2,7 +2,7 @@ import Page from "@/components/page/Page";
 import { DEFAULT_LOCALE, LOCALES } from "@/const/locales";
 import { fetchPages } from "@/fetch/fetchPages";
 
-export const revalidate = 3600;
+export const revalidate = false;
 export const dynamicParams = true;
 
 export async function generateStaticParams() {
--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import Page from "@/components/page/Page";
 import { LOCALES } from "@/const/locales";
 
-export const revalidate = 3600;
+export const revalidate = false;
 
 export async function generateStaticParams() {
   return LOCALES.map((locale) => ({ locale }));
